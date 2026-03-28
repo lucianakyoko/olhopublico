af9fb7759d4114219c9b1d0b7d4a21a399797ebf
Luciana Kyoko <lukyoko@protonmail.com>
2026-03-28 17:00:16 -0300

    feat(components/politics): add PoliticsPagination component

---
+++ src/components/politics/PoliticsPagination.tsx
@@ -0,0 +1,46 @@
+// src/components/politicos/PoliticosPagination.tsx
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+
+export function PoliticsPagination() {
+  return (
+    <div className="flex justify-center mt-12">
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+
+          <PaginationItem>
+            <PaginationLink href="#" isActive>1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">2</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">3</PaginationLink>
+          </PaginationItem>
+
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+
+          <PaginationItem>
+            <PaginationLink href="#">18</PaginationLink>
+          </PaginationItem>
+
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
+}
